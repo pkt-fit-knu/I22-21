089766750798167120bab63cf281aa6f3713e197
Dovbnia Dima <dima---07@mail.ru>
2016-03-14 23:02:06 +0200

    Added files via upload

---
+++ Bilinear_interpolation/Bilinear_interpolation/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Bilinear_interpolation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.new_img_box = new Emgu.CV.UI.ImageBox();
+            this.zoom_img_block = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.new_img_box)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zoom_img_block)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // new_img_box
+            // 
+            this.new_img_box.Location = new System.Drawing.Point(12, 12);
+            this.new_img_box.Name = "new_img_box";
+            this.new_img_box.Size = new System.Drawing.Size(500, 500);
+            this.new_img_box.TabIndex = 30;
+            this.new_img_box.TabStop = false;
+            // 
+            // zoom_img_block
+            // 
+            this.zoom_img_block.Location = new System.Drawing.Point(521, 12);
+            this.zoom_img_block.Name = "zoom_img_block";
+            this.zoom_img_block.Size = new System.Drawing.Size(500, 500);
+            this.zoom_img_block.TabIndex = 31;
+            this.zoom_img_block.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1033, 522);
+            this.Controls.Add(this.zoom_img_block);
+            this.Controls.Add(this.new_img_box);
+            this.Name = "Form1";
+            this.Text = "Білінійна інтерполяція";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.new_img_box)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zoom_img_block)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox new_img_box;
+        private Emgu.CV.UI.ImageBox zoom_img_block;
+    }
+}
+
---
+++ Bilinear_interpolation/Bilinear_interpolation/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Npgsql;
+using Emgu.CV.UI;
+
+namespace Bilinear_interpolation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Image<Gray, Byte> imgProcessed;
+            string src = @"Bilinear_interpolation.png";
+            Image<Bgr, Byte> newimgOriginal;
+
+            newimgOriginal = new Image<Bgr, byte>((Bitmap)Bitmap.FromFile(src));
+
+            imgProcessed = newimgOriginal.InRange(new Bgr(0, 0, 0), new Bgr(100, 100, 100));
+
+            //newimgOriginal[1, 1] = newimgOriginal[1, 1];// newimgOriginal[]
+
+            int k = 3;
+
+            Image<Bgr, Byte> zoom_img = new Image<Bgr, byte>(imgProcessed.Width * k, imgProcessed.Height * k);
+
+            for (int i = 0; i < imgProcessed.Height * k; i++)
+            {
+                for (int j = 0; j < imgProcessed.Width * k; j++)
+                {
+                    if (i % k == 0)//i
+                    {
+                        if (j % k == 0)//j
+                        {
+                            zoom_img[i, j] = newimgOriginal[i / k, j / k];
+                        }
+                        else// j%k+1
+                        {
+                            Bgr color_l = newimgOriginal[i / k, j / k];
+                            Bgr color_r;
+                            if ((j / k) + 1 < newimgOriginal.Width)
+                                color_r = newimgOriginal[i / k, (j / k)+1];
+                            else
+                                color_r = color_l;
+                            Bgr bgr = new Bgr((color_l.Blue + color_r.Blue) / 2, (color_l.Green + color_r.Green) / 2, (color_l.Red + color_r.Red) / 2);
+                            zoom_img[i, j] = bgr;
+                        }
+                    }
+                    else //i
+                    {
+                        if (j % k == 0)//j
+                        {
+                            Bgr color_l = newimgOriginal[i / k, j / k];
+                            Bgr color_r;
+                            if ((i / k) + 1 < newimgOriginal.Height)
+                                color_r = newimgOriginal[Convert.ToInt32((i / k) + 1), j / k];
+                            else
+                                color_r = color_l;
+                            Bgr bgr = new Bgr((color_l.Blue + color_r.Blue) / 2, (color_l.Green + color_r.Green) / 2, (color_l.Red + color_r.Red) / 2);
+                            zoom_img[i, j] = bgr;
+                        }
+                        else //j
+                        {
+                            Bgr color_l_t = newimgOriginal[i / k, j / k];
+                            Bgr color_r_t;
+                            if ((i / k) + 1 < newimgOriginal.Height)
+                                color_r_t = newimgOriginal[(i / k) + 1, j / k];
+                            else
+                                color_r_t = color_l_t;
+                            Bgr bgr_top = new Bgr((color_l_t.Blue + color_r_t.Blue) / 2, (color_l_t.Green + color_r_t.Green) / 2, (color_l_t.Red + color_r_t.Red) / 2);
+
+                            Bgr bgr_bottom;
+                            if ((j / k) + 1 < newimgOriginal.Width)
+                            {
+                                Bgr color_l_b = newimgOriginal[i / k, (j / k) + 1];
+                                Bgr color_r_b;
+                                if ((i / k) + 1 < newimgOriginal.Height)
+                                    color_r_b = newimgOriginal[(i / k) + 1, (j / k) + 1];
+                                else
+                                    color_r_b = color_l_b;
+                                bgr_bottom = new Bgr((color_l_b.Blue + color_r_b.Blue) / 2, (color_l_b.Green + color_r_b.Green) / 2, (color_l_b.Red + color_r_b.Red) / 2);
+                            }
+                            else
+                                bgr_bottom = bgr_top;
+
+                            Bgr bgr = new Bgr((bgr_top.Blue + bgr_bottom.Blue) / 2, (bgr_top.Green + bgr_bottom.Green) / 2, (bgr_top.Red + bgr_bottom.Red) / 2);
+
+                            zoom_img[i, j] = bgr;
+                        }
+                    }
+                }
+            }
+            // imgProcessed = matrix_new.ToImage;
+            // CvInvoke.ma
+            //Image<Bgr, Byte> img = matrix_new.QueryFrame().ToImage<Bgr, Byte>();
+
+
+            new_img_box.Image = newimgOriginal;
+            this.zoom_img_block.Image = zoom_img;
+
+
+
+
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
